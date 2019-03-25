1d8cc08dd8e873f5a773cde641805601399292ca
Nicolas Lopierre Aguirre <Nlopierre@users.noreply.github.com>
2019-03-25 18:15:49 -0400

    Update test.sh

--- bin/test.sh
+++ bin/test.sh
@@ -5,7 +5,6 @@ set -e
 ganache-cli --gasLimit 6721975 2> /dev/null 1> /dev/null &
 sleep 5 # to make sure ganache-cli is up and running before compiling
 rm -rf build
-cd bcContracts
 truffle compile
 truffle migrate --reset --network development
 truffle test
