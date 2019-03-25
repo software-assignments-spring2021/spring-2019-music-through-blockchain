c9de8cfbd95cac2c111c4f61940904ddcde4452a
Kalea Palmer <kaleapalmer@gmail.com>
2019-03-25 16:28:20 -0400

    Creating the form for upload song

---
+++ src/containers/SongUpload.js
@@ -0,0 +1,215 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import Paper from '@material-ui/core/Paper'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import { withStyles } from '@material-ui/core/styles'
+import {signUp} from '../store/actions/authActions'
+import { Grid } from '@material-ui/core'
+import { Redirect } from 'react-router-dom'
+import {SignupComponent} from "./SignupComponent";
+
+
+const styles = (theme) => ({
+    textField: {
+        minWidth: 280,
+        marginTop: 20
+    },
+    contain: {
+        margin: '0 auto'
+    },
+    paper: {
+        minHeight: 370,
+        maxWidth: 450,
+        minWidth: 337,
+        textAlign: 'center',
+        display: 'block',
+        margin: 'auto'
+    }
+})
+
+export class SongUploadComponent extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            songNameInput: '',
+            songNameInputError: '',
+            artistNameInput: '',
+            artistNameInputError: '',
+            priceInput: '',
+            priceInputError: ''
+        }
+        this.handleForm = this.handleForm.bind(this)
+    }
+
+    handleInputChange = (event) => {
+        const target = event.target
+        const value = target.type === 'checkbox' ? target.checked : target.value
+        const name = target.name
+        this.setState({
+            [name]: value
+        })
+
+        switch (name) {
+            case 'songNameInput':
+                this.setState({
+                    songNameInputError: ''
+                })
+                break
+            case 'artistNameInput':
+                this.setState({
+                    artistNameInputError: ''
+                })
+                break
+            case 'priceInput':
+                this.setState({
+                    priceInputError: ''
+                })
+                break
+            default:
+
+        }
+    }
+
+    handleForm = () => {
+
+        const { songNameInput, artistNameInput, priceInput}= this.state
+        const { register } = this.props
+
+        let error = false
+
+        // Validate full name
+        let songNameCheck = songNameInput.trim().toLowerCase()
+
+        if (songNameCheck.length < 2) {
+            this.setState({
+                songNameInputError: 'Please enter a valid song name'
+            })
+            error = true
+        }
+
+        /* Check artist name */
+        if (artistNameInput === '') {
+            this.setState({
+                artistNameInputError: 'Please enter a valid artist name'
+            })
+            error = true
+        }
+
+       /* Check artist name*/
+        let intPrice = parseInt(priceInput)
+
+        if (isNaN(intPrice)) {
+            this.setState({
+                priceInputError: 'Please enter a valid a price'
+            })
+            error = true
+        }
+
+        if (isNaN(intPrice)) {
+            if (intPrice && intPrice > 1) {
+                this.setState({
+                    priceInputError: 'Please enter a valid a price'
+                })
+                error = true
+            }
+        }
+
+        //    
+        // if (intPrice && intPrice > 1) {
+        //     this.setState({
+        //         priceInputError: 'Please enter a valid a price'
+        //     })
+        //     error = true
+        // }
+
+        if (!error) {
+            register({
+                title: songNameInput,
+                artistName: artistNameInput,
+                price: priceInput,
+            })
+        }
+    }
+
+    render() {
+
+        const { classes, auth } = this.props
+        // if (auth.uid){
+        //     this.props.history.push('/')
+        //     return <Redirect to='/' />
+        // }
+
+        return (
+            <Grid container spacing={24}>
+                <Grid item xs={12} className={classes.contain}>
+                    <div>
+                        <Paper className={classes.paper} elevation={1} >
+                            <div style={{ padding: '48px 40px 36px' }}>
+                                <div style={{ paddingLeft: '40px', paddingRight: '40px'}}>
+                                    <h2>bMusic Sign Up</h2>
+                                </div>
+                                <TextField
+                                    className={classes.textField}
+                                    autoFocus
+                                    onChange={this.handleInputChange}
+                                    helperText={this.state.songNameInputError}
+                                    error={this.state.songNameInputError.trim() !== ''}
+                                    name='songNameInput'
+                                    label='Song Name'
+                                    type='text'
+                                /><br />
+                                <TextField
+                                    className={classes.textField}
+                                    autoFocus
+                                    onChange={this.handleInputChange}
+                                    helperText={this.state.artistNameInputError}
+                                    error={this.state.artistNameInputError.trim() !== ''}
+                                    name='artistNameInput'
+                                    label='Artist Name'
+                                    type='text'
+                                /><br />
+                                <TextField
+                                    className={classes.textField}
+                                    onChange={this.handleInputChange}
+                                    helperText={this.state.priceInputError}
+                                    error={this.state.priceInputError.trim() !== ''}
+                                    name='priceInput'
+                                    label='Price'
+                                    type='text'
+                                /><br />
+                                <br />
+                                <div>
+                                    <div>
+                                        <Button variant='contained' color='primary' onClick={this.handleForm}>Upload Song</Button>
+                                    </div>
+                                </div>
+                            </div>
+                        </Paper>
+                    </div>
+                </Grid>
+            </Grid>
+        )
+
+    }
+
+}
+
+const mapStateToProps = (state) => {
+    return {
+        auth: state.firebase.auth,
+        profile: state.firebase.profile
+    }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        // register: (userRegister) => {
+        //     dispatch(signUp(userRegister))
+        // },
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(SongUploadComponent))
