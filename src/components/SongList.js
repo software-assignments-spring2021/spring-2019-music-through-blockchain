ab546de5de53f20920867cf6c021eaa0978c213a
Pham Phuong Anh Dang <ann0605@10-17-132-255.dynapool.wireless.nyu.edu>
2019-04-03 18:21:33 -0400

    remove not working functions

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
@@ -107,18 +106,5 @@ export class SongList extends Component {
     }}
 
 
-const mapStateToProps = (state) => {
-  return {
-    songs: state.firestore.ordered.songs
-  }
-}
 
-
-export default compose(
-    connect(mapStateToProps),
-    firestoreConnect([{
-      collection: 'songs'
-     // ordrerBy
-    }]), 
-    withStyles(styles)
-  )(SongList)
+export default withStyles(styles)(SongList);
