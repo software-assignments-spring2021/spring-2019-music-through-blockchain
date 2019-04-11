23f17d6d50f2ca3290ae66c44fcec5c081863a19
Pham Phuong Anh Dang <ann0605@Phams-MacBook-Pro-2.local>
2019-04-11 00:50:35 -0400

    added routing to individual song page from song list

--- src/containers/mock.js
+++ src/containers/mock.js
@@ -2,12 +2,14 @@ import coverArt from '../img/albumArt.png'
 import coverArtTwo from '../img/tameImpala.jpg'
 export const songs =[
     {
+        id: 'fl30BfSLL3WLpJIu5T0I',
         title: 'song1',
         coverArt: coverArt ,
         artist: ' artist name',
         prices: {1: [5,5]}
     }, 
     {
+        id: 'Qofkm9G9zLUWUU9hrUCC', 
         title: 'song2',
         coverArt: coverArt ,
         artist: ' artist name',
