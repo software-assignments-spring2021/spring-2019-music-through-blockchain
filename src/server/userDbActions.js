58914caff577074fbf6fab62f4a42463a593cf3c
Kalea Palmer <kaleapalmer@gmail.com>
2019-04-18 01:15:39 -0400

    edit profile now deletes the existing profile picture, work on object-not-found error handling next

--- src/server/userDbActions.js
+++ src/server/userDbActions.js
@@ -39,32 +39,59 @@ dbGetUserInfo: (userId) => {
 
     updateProfileWithImage: (uid, profile, image, imageName) => {
         return new Promise((resolve, reject) => {
-            // const batch = db.batch()
             let profileData = {}
-            console.log(uid)
             let profileRef = db.doc(`users/${uid}`)
 
-            //ADD IMAGE TO THE PROFILE if its changed
-            const imageKey = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 9)
-            const imageStorageFile = storageRef.child(`images/${imageKey}_${imageName}`)
+            //find the existing profile picture saved in storage
+            profileRef.get().then((profileDoc) => {
+                const existingImagePath = profileDoc.data().imageFullPath
+                const existingImageRef = storageRef.child(existingImagePath)
 
-            console.log(profile)
+                //delete the existing profile picture
+                existingImageRef.delete().then(() => {
+                    console.log("deleted existing profile picture")
+                    //save the new chosen profile picture into storage
+                    const imageKey = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 9)
+                    const imageStorageFile = storageRef.child(`images/${imageKey}_${imageName}`)
 
-            imageStorageFile.put(image).then((imageResult) => {
-                imageResult.ref.getDownloadURL().then((imageDownloadURL) => {
-                    profileData = {
-                        ...profile,
-                        photoUrl: imageDownloadURL,
-                        imageFullPath: imageResult.metadata.fullPath
-                    }
-                    profileRef.update(profileData).then(() => {
-                        resolve(profileData)
-                    }).catch((error) => {
-                        reject(error)
+                    imageStorageFile.put(image).then((imageResult) => {
+                        imageResult.ref.getDownloadURL().then((imageDownloadURL) => {
+                            profileData = {
+                                ...profile,
+                                photoUrl: imageDownloadURL,
+                                imageFullPath: imageResult.metadata.fullPath
+                            }
+                            //save the new profile information and image path to database
+                            profileRef.update(profileData).then(() => {
+                                resolve(profileData)
+                            }).catch((error) => {
+                                reject(error)
+                            })
+                        })
                     })
                 })
-
             })
+
+            //ADD IMAGE TO THE PROFILE if its changed
+            // const imageKey = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 9)
+            // const imageStorageFile = storageRef.child(`images/${imageKey}_${imageName}`)
+            //
+            // console.log(profile)
+            //
+            // //retrieve the old image path and then save the new image
+            // imageStorageFile.put(image).then((imageResult) => {
+            //     imageResult.ref.getDownloadURL().then((imageDownloadURL) => {
+            //         profileData = {
+            //             ...profile,
+            //             photoUrl: imageDownloadURL,
+            //             imageFullPath: imageResult.metadata.fullPath
+            //         }
+            //         profileRef.update(profileData).then(() => {
+            //
+            //         })
+            //     })
+            //
+            // })
 
             // profileRef.update(profile).then(() => {
             //     //if there is an image to save
