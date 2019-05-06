bb82ad51ca979be64899a71e0b2ad410239d62e5
Kalea Palmer <kaleapalmer@gmail.com>
2019-05-06 17:42:39 -0400

    Added a howto page to the navbar

--- src/components/SignedOutLinks.js
+++ src/components/SignedOutLinks.js
@@ -6,6 +6,7 @@ const SignedOutLinks = () => {
         <div style={{position: 'relative', top: 55, }}>
             <h3 style={{display:'inline', padding: 15}}><NavLink to='/signup' style={{textDecoration: 'none', color: '#636161'}}>Sign Up</NavLink></h3>
             <h3 style={{display:'inline', padding: 15}}><NavLink to='/signin' style={{textDecoration: 'none', color: '#636161'}}>Log In</NavLink></h3>
+            <h3 style={{display:'inline', padding: 15}}><NavLink to='/howto' style={{textDecoration: 'none', color: '#636161'}}>How to Get Started</NavLink></h3>
         </div>
     )
 
